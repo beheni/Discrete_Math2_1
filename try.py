0ebc048ad491acd0c68d419866369731d9bbbf55
ViktoryaK <vkocherkevuch@gmail.com>
2022-02-21 20:30:29 +0200

    version 32454657

--- try.py
+++ try.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import cross_val_score
@@ -9,34 +9,26 @@ iris = load_iris()
 
 class Node:
 
-
-    def __init__(self, X=None, y=None, depth=0, gini=10, feature_index=0, threshold=0, left=None, right=None, value=None):
+    def __init__(self, X, y, gini=0, feature_index=0, threshold=0, left=None, right=None):
         self.X = X
         self.y = y
-        self.depth = depth
         self.gini = gini
         self.feature_index = feature_index
         self.threshold = threshold
         self.left = left
         self.right = right
-        self.value = value
-        self.indent = " "
 
-    def __str__(self):
-        return f"{self.depth*'  '}{self.gini}, {self.depth}, {self.feature_index}, {self.threshold}"
 
 class MyDecisionTreeClassifier:
 
     def __init__(self, max_depth):
         self.max_depth = max_depth
-        self.root = None
-        self.value = None
+        self.root = 0
     def gini(self, y):
         """
         classes = [[0],[1],[2]]
         A Gini score gives an idea of how good a split is by how mixed the
         classes are in the two groups created by the split.
-
         A perfect separation results in a Gini score of 0,
         whereas the worst case split that results in 50/50
         classes in each group result in a Gini score of 0.5
@@ -57,7 +49,7 @@ class MyDecisionTreeClassifier:
 
 
     def split_data(self, X, y):
-        IG = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        IG = [0, 0, 0, 0, 0, 0, 0, 0]
         num_features = iris.feature_names
         for feature_index in range(len(num_features)):
             possible_thresholds = sorted(np.unique(X[:, feature_index]), reverse=True)
@@ -95,36 +87,31 @@ class MyDecisionTreeClassifier:
                         IG[5] = feature_index
                         IG[6] = y_left
                         IG[7] = y_right
-                        IG[8] = gini_p
         return IG
 
-    def build_tree(self, node):
-        X = node.X
-        y = node.y
-        depth = node.depth
-        if node != None:
+    def build_tree(self, X, y, depth=0, nodes=[], leaves=[]):
+        if len(set(y)) > 1:
             if depth <= self.max_depth:
                 split = self.split_data(X, y)
-                if split[8] == 0:
-                    node.value = f"A leaf with {split[3]} flowers, threshold = {split[4]}, feature = {split[5]}"
                 if split[0] > 0:
-                    node.left = self.build_tree(Node(X=split[1], y=split[6], depth=depth + 1, gini=split[0], feature_index=split[5], threshold=split[4]))
-                    print(node.left)
-                    node.right = self.build_tree(Node(X=split[2], y=split[7], depth=depth + 1, gini=split[0], feature_index=split[5], threshold=split[4]))
-                    print(node.right)
-                    return node
+                    nodes.append(Node(X, y, split[0], split[5], split[4]))
+                    left_son = self.build_tree(split[1], split[6], depth + 1)
+                    right_son = self.build_tree(split[2], split[7], depth + 1)
+        else:
+            nodes.append(Node(X=X, y=iris.target_names[int(y[0])]))
+            leaves.append(Node(X=X, y=iris.target_names[int(y[0])]))
+        return nodes
+
+    def print_tree(self, X, y, indent=" "):
+        self.root = self.build_tree(X, y)
+        for node in self.root:
+            print(f"{iris.feature_names[node.feature_index]} <= {node.threshold}" + '\n' + f'gini = {node.gini}' + '\n' + f"samples = {len(node.X)}" + '\n' + f"name {node.y}")
+
+    def fit(self, X, y):
+        # basically wrapper for build tree
+
+        pass
 
 
-    def fit(self, tree):
-        ''' function to train the tree '''
-        if not tree:
-            tree = self.root
-        if tree.value is not None:
-            print(tree.value)
-        tr = self.split_data(iris.data, iris.target)
-        tree_nd_class = Node(tr[3], iris.target, 0, tr[0], tr[5], tr[4], left=None, right=None)
-        self.root = self.build_tree(tree_nd_class)
-
-if __name__ == "__main__":
-    my_tree = MyDecisionTreeClassifier(10)
-    tree = my_tree.fit(my_tree)
+my_tree = MyDecisionTreeClassifier(10)
+print(my_tree.print_tree(iris.data[:-38], iris.target[:-38]))
